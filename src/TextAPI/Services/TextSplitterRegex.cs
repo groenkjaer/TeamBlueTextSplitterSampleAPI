60114639c8d5738fec044a7c99985e2c5fb2d573
groenkjaer <FillipJensen@live.dk>
2022-10-13 15:52:46 +0200

    Added textsplitter implementation with Regex, and some tests for this

---
+++ src/TextAPI/Services/TextSplitterRegex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TextAPI.Services
+{
+    public class TextSplitterRegex : ITextSplitter
+    {
+        private Regex Regex { get; init; }
+
+        public TextSplitterRegex()
+        {
+            Regex = new(@"\w+");
+        }
+
+        public IEnumerable<string> SplitIntoDistinctWords(string text)
+        {
+            return Regex.Matches(text).Select(m => m.Value).Distinct();
+        }
+    }
+}
---
+++ test/TextAPI.Tests/TextSplitterRegexTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextAPI.Services;
+
+namespace TextAPI.Tests
+{
+    public class TextSplitterRegexTests
+    {
+        private ITextSplitter regexSplitter;
+
+        [SetUp]
+        public void Setup()
+        {
+            regexSplitter = new TextSplitterRegex();
+        }
+
+        [Test]
+        public void SplitUniqueWords()
+        {
+            var uniqueWords = regexSplitter.SplitIntoDistinctWords("a.horse,and;a2dog");
+            Assert.That(uniqueWords.Count(), Is.EqualTo(4));
+        }
+    }
+}
